11251733744f672d613f356055f1408766516b7f
_woo_s.j <91654658+sjwoo1999@users.noreply.github.com>
2024-01-05 10:26:56 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,15 @@
+function solution(num) {
+    var count = 0;
+    
+    while (num !== 1){
+        if (num % 2 === 0)
+            num /= 2;
+        else if (num % 2 !== 0)
+            num = num * 3 + 1;
+        count++;
+    }
+    
+    if (count > 500) return -1;
+    
+    return count;
+}
